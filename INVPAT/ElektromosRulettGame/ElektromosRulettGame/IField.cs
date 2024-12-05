0f1a6bb967c07769f6757f12173275e2e34997b7
TiberiusMagic <125692444+TiberiusMagic@users.noreply.github.com>
2024-12-05 16:19:14 +0100

    Elso intefesz

---
+++ INVPAT/ElektromosRulettGame/ElektromosRulettGame/IField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektromosRulettGame
+{
+    internal interface IField
+    {
+        int getNumber();
+        int setNumber(int number);
+        string getColor();
+        string setColor(string color);
+        string toString();
+    }
+}
